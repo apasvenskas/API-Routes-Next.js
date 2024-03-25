8f8c79cdc09c7bbff3661548b3d7cc0eec2aea7d
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-03-24 22:56:06 -0700

    added a load feature to get data

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,27 +1,38 @@
-import fs from 'fs'; 
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-function handler(req, res){
-    if(req.method === 'POST'){
-        const email = req.body.email; 
-        const feedbackText = req.body.text; 
-
-        const newFeedback = {
-            id: new Date().toISOString,
-            email: email,
-            text: feedbackText
-        }
-        // store it in a database, in this case it will be a file in a data folder
-        const filePath = path.join(process.cwd(), 'data', 'feedback.json'); // get the current working directory with process.cwd()
-        const fileData = fs.readFileSync(filePath); // read data stored in a file
-        // convert the data from json to js object
-        const data = JSON.parse(fileData); 
-        data.push(newFeedback);
-        fs.writeFileSync(filePath, JSON.stringify(data)); 
-        res.status(201).json({ message: 'Success!', feedback: newFeedback })
-    } else {
-        res.status(200).json({ message: 'This works!' });
-    }
+function buildFeedbackPath() {
+  return path.join(process.cwd(), "data", "feedback.json"); // get the current working directory with process.cwd()
 }
 
-export default handler; 
+function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath); // read data stored in a file
+  // convert the data from json to js object
+  const data = JSON.parse(fileData);
+  return data; 
+}
+
+function handler(req, res) {
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString,
+      email: email,
+      text: feedbackText,
+    };
+    // store it in a database, in this case it will be a file in a data folder
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    res.status(200).json({ feedback: data });
+  }
+}
+
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import { Fragment } from "react";
-import { useRef } from 'react'; 
+import { useRef, useState } from 'react'; 
 
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
 
@@ -23,6 +24,13 @@ function HomePage() {
     .then((data) => console.log(data)); 
   }
 
+  function loadFeedbackHandler(){
+    fetch('/api/feedback') // send the form to api folder
+    .then((response) => response.json())
+    .then((data) => {setFeedbackItems(data.feedback)
+    }); 
+  }
+
   return (
     <> {/* Fragment is used to avoid unnecessary wrapper */}
       <div>
@@ -38,6 +46,13 @@ function HomePage() {
           </div>
           <button>Send Feedback</button>
         </form>
+        <hr />
+        <button onClick={loadFeedbackHandler} >Load Feedback</button>
+        <ul>
+          {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
       </div>
     </>
   )
