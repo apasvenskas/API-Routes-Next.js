767bb7621452d29108bf78a7c44fd7908785a8e2
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-04-06 12:49:09 -0700

    featured post implemented

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,39 @@
-import Hero from '../components/home-page/hero';
-import { Fragment, useRef, useState } from 'react'; 
+import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
+
+import { Fragment, useRef, useState } from "react";
+
+const DUMMY_POSTS = [
+  {
+    title: "Dummy Post",
+    image: "getting-started-nextjs.png",
+    excerpt: "Testing the post functionality",
+    date: "2024-04-06",
+    slug: "getting-started-with-nextjs",
+  },
+  {
+    title: "Dummy Post2",
+    image: "nextjs-file-based-routing.png",
+    excerpt: "Testing the post functionality",
+    date: "2024-04-06",
+    slug: "getting-started-with-nextjs",
+  },
+  {
+    title: "Dummy Post",
+    image: "mastering-js-thumb.png",
+    excerpt: "Testing the post functionality",
+    date: "2024-04-06",
+    slug: "mastering-javascript",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
-  )
+  );
 }
 
 export default HomePage;
