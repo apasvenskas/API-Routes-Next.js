8e2f0eb98c0ccdfb404f58da2bbae09b2048a4ff
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-04-11 15:45:59 -0700

    finished image formating in the post tab

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,17 +1,41 @@
-import ReactMarkdown from 'react-markdown';
-import PostHeader from './post-header';
-import classes from './post-content.module.css'
+import ReactMarkdown from "react-markdown";
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css";
+import Image from "next/image";
 
-function PostContent(props){
-  const {post} = props;
-  const imagePath = `/images/posts/${post.slug}/${post.image}`; 
-  
-  return <article className={classes.content}>
-    <PostHeader title={post.title} image={imagePath} />
-    <ReactMarkdown>
-      {post.content}
-    </ReactMarkdown>
-  </article>
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  // image renderers (could be done with css additional feature)
+  const customRenderers = {
+   
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>
+    },
+  };
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
+    </article>
+  );
 }
 
 export default PostContent;
